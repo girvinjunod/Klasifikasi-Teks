9c1d873179f7d0db070b235da3aa0c04dbe2f8d6
Isabella Handayani Sumantri <isabellahandayani@gmail.com>
2022-09-16 22:37:24 +0700

    chore: pre-commit

--- data_worthcheck/check.py
+++ data_worthcheck/check.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-ps = pd.read_csv('dev.csv')
-dtext_a = list(ps['text_a'])
-ps = pd.read_csv('train.csv')
-ttext_a = list(ps['text_a'])
-tlabel = list(ps['label'])
+ps = pd.read_csv("dev.csv")
+dtext_a = list(ps["text_a"])
+ps = pd.read_csv("train.csv")
+ttext_a = list(ps["text_a"])
+tlabel = list(ps["label"])
 
 text_a = []
 label = []
@@ -17,6 +17,6 @@ for t, l in zip(ttext_a, tlabel):
         label.append(l)
 if count:
     print(len(ttext_a), count, len(text_a))
-    dic = {'text_a': text_a, 'label': label}
+    dic = {"text_a": text_a, "label": label}
     ps = pd.DataFrame(dic)
-    ps.to_csv('train_filtered.csv')
+    ps.to_csv("train_filtered.csv")
--- shallow.py
+++ shallow.py
@@ -1,33 +1,35 @@
-from sklearn.model_selection import train_test_split
+import pandas as pd
 from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer
 from xgboost import XGBClassifier
-from sklearn.model_selection import GridSearchCV
-import pandas as pd
 
 # Memakai XGBoost
 
 maps = {"no": 0, "yes": 1}
 
-path = 'data_worthcheck/'
-data_train = pd.read_csv(path+'train.csv')
-data_test = pd.read_csv(path+'test.csv')
+path = "data_worthcheck/"
+data_train = pd.read_csv(path + "train.csv")
+data_test = pd.read_csv(path + "test.csv")
 
 
-vectorizer = CountVectorizer(
-    token_pattern="[A-Za-z_][A-Za-z\d_]*", min_df=0.001)
+vectorizer = CountVectorizer(token_pattern=r"[A-Za-z_][A-Za-z\d_]*", min_df=0.001)
 X_train = vectorizer.fit_transform(data_train.text_a).toarray()
-y_train = data_train['label']
+y_train = data_train["label"]
 
 X_test = vectorizer.transform(data_test.text_a).toarray()
-y_test = data_test['label']
+y_test = data_test["label"]
 
 y_train = y_train.replace(maps)
 y_test = y_test.replace(maps)
 
-model = XGBClassifier(n_estimators=500, tree_method='hist',
-                      subsample=0.8, random_state=1,
-                      eta=0.3, max_depth=18)
+model = XGBClassifier(
+    n_estimators=500,
+    tree_method="hist",
+    subsample=0.8,
+    random_state=1,
+    eta=0.3,
+    max_depth=18,
+)
 
 model.fit(X_train, y_train)
 
@@ -39,10 +41,10 @@ prec = metrics.precision_score(y_test, y_pred)
 rec = metrics.recall_score(y_test, y_pred)
 f1 = metrics.f1_score(y_test, y_pred)
 acc = metrics.accuracy_score(y_test, y_pred)
-print('Precision Score: {:.4f}'.format(prec))
-print('Recall Score: {:.4f}'.format(rec))
-print('F1 Score: {:.4f}'.format(f1))
-print('Accuracy Score: {:.4f}'.format(acc))
+print("Precision Score: {:.4f}".format(prec))
+print("Recall Score: {:.4f}".format(rec))
+print("F1 Score: {:.4f}".format(f1))
+print("Accuracy Score: {:.4f}".format(acc))
 
 
 # Grid Search for best Params
